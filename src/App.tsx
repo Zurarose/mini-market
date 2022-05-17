cce2caeacfb45725454de439569845b2ab8877d8
Олег Санников <specter8471102@gmail.com>
2022-05-17 16:29:24 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import defaultTheme from "./Common/MaterialTheme/theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import RootComponent from "./Components/RootComponent";
+import {UIContextProvider} from "./Common/UIContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline/>
+            <UIContextProvider>
+                <RootComponent/>
+            </UIContextProvider>
+        </ThemeProvider>
+    );
 }
-
 export default App;
---
+++ src/Common/UIContext/index.tsx
@@ -0,0 +1,47 @@
+import {Alert, Snackbar} from "@mui/material";
+import React, {createContext, useState} from "react";
+
+export const UIContext = createContext<UIContextPropsAlert>({} as UIContextPropsAlert);
+
+type AlertColor = 'success' | 'info' | 'warning' | 'error';
+
+interface UIContextTypeAlert {
+    show: boolean;
+    severity?: AlertColor;
+    message?: string;
+}
+
+interface UIContextPropsAlert {
+    setAlert: React.Dispatch<React.SetStateAction<UIContextTypeAlert>>;
+}
+
+interface PropTypes {
+    children?: React.ReactNode
+}
+
+export const UIContextProvider: React.FC<PropTypes> = ({children}) => {
+    const [alert, setAlert] = useState<UIContextTypeAlert>({
+        show: false,
+        severity: 'info',
+        message: '',
+    });
+    const handleCloseAlert = () =>
+        setAlert({
+            show: false,
+        });
+
+    return (
+        <UIContext.Provider value={{setAlert}}>
+            {children}
+            <Snackbar
+                open={alert.show}
+                autoHideDuration={4000}
+                onClose={handleCloseAlert}
+            >
+                <Alert elevation={6} variant="filled" severity={alert.severity}>
+                    {alert.message}
+                </Alert>
+            </Snackbar>
+        </UIContext.Provider>
+    );
+};
